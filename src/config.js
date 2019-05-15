8dd36080abd57f228b6c730ddaca919929858b9a
chadwhittaker <chadwhit124@gmail.com>
2019-05-15 11:43:29 -0400

    change backend to http instead of https

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://backend.chad-whittaker.com`;
+export const prodEndpoint = `http://backend.chad-whittaker.com`;
 // export const prodEndpoint = `https://halo-rank-yoga-prod.herokuapp.com/`;
